4c0ebcaa3a2e2315f9ca9b6d27ee1e5f2f0c72fc
thomasbjorndahl <Thomas.Bjorndahl@technipfmc.com>
2018-10-29 10:49:44 +0100

    Added ADMembers

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ A (soon to be) collection of usefull tools
 
 - **dbping**
 _Tool used to ping a database_
+- **ADMembers**
+_Tool for checking AD membership_
---
+++ admembers/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using ADMembers.Internals;
+
+namespace ADMembers
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            Application.Current.DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception exc = e.ExceptionObject as Exception;
+            if(null != exc)
+            {
+                Logger.Log(exc);
+            }
+            
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Logger.Log(e.Exception);
+            e.Handled = true;
+        }
+    }
+}
